e055500139e75819b0ef2f1dd113a24c1d6888a3
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2026-09-01 21:02:46 -0300

    fixed

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Despesas - Centenário do Sul",
+    page_title="Auditoria de Empenho, Liquidação e Pagamento",
     page_icon="🧾",
     layout="wide",
 )
 
 pages = {
-    "➡️ Realizável": [
-        st.Page("pages/liquidado_x_pago.py", title="Liquidado x Pago"),
+    "➡️ SCP": [
+        st.Page("pages/01_empenho.py", title="Empenho"),
+        st.Page("pages/02_liquidacao.py", title="Liquidação"),
+        st.Page("pages/03_pagamento.py", title="Pagamento"),
+    ],
+
+    "➡️ Bancos": [
+        st.Page("pages/04_extrato.py", title="Extrato"),
+    ],
+
+    "⚙️ Sistema": [
         st.Page("pages/00_fonte_dados.py", title="Fonte de Dados"),
     ],
+
 }
 
 pg = st.navigation(pages)
